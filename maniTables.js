03de3e3cfbbad3e4a2249d2a5e0cbb80119ee0e1
cupham211 <37853157+cupham211@users.noreply.github.com>
2021-03-02 13:06:28 -0800

    fixed put for employees

--- maniTables.js
+++ maniTables.js
@@ -53,47 +53,62 @@ function createAffiliateTable(json){
   tableBod.innerHTML += row;
 }
 
-function createEmployeeTable(json){
+function createEmployeeTable(emp, positions, depts, quotes){
   var tableBod = document.getElementById('employeeTableBod');
   var row = '';
 
-  for (i=0; i<json.length; i++){
-    row += ` <tr scope="col" id="${i}${json[i].employeeID}">
-          <td>${json[i].employeeID}</td>
-          <td class="checkFullName" style="display:none;">${json[i].fName} ${json[i].lName}</td>
-          <td>${json[i].fName}</td>
-          <td>${json[i].lName}</td>
-          <td>${json[i].alias}</td>
-          <td><span class="spanrow${i}"  style="display:inline;">${json[i].title}</span>
-            <div class="input-group mb-3 update${i}" style="display:none;">
-              <select class="form-select" id="${i}posID">
-                <option value="${json[i].positionID}" selected>${json[i].title}</option>
-                <option value="-1">NULL</option>
+  for (i=0; i<emp.length; i++){
+    row += ` <tr scope="col" id="${i}${emp[i].employeeID}">
+          <td>${emp[i].employeeID}</td>
+          <td class="checkFullName" style="display:none;">${emp[i].fName} ${emp[i].lName}</td>
+          <td>${emp[i].fName}</td>
+          <td>${emp[i].lName}</td>
+          <td>${emp[i].alias}</td>
+          <td><span class="spanrow${i}"  style="display:inline;">${emp[i].title}</span>
+
+              <select class="form-select update${i}" style="display:none;" id="${i}posID">
+                <option value="${emp[i].positionID}" selected>${emp[i].title}</option>`;
+                for (j=0; j<positions.length; j++){
+                  if (emp[i].positionID != positions[j].positionID) {
+                    row += `<option value="${positions[j].positionID}">${positions[j].title}</option>`;
+                  }
+                }
+          row +=  `<option value="-1">NULL</option>
               </select>
-            </div>
+
           </td>
-          <td><span class="spanrow${i}" style="display:inline;">${json[i].deptName}</span>
-            <div class="input-group mb-3 update${i}" style="display:none;">
-              <select class="form-select" id="${i}deptID">
-                <option value="${json[i].departmentID}" selected>${json[i].deptName}</option>
-                <option value="-1">NULL</option>
+          <td><span class="spanrow${i}" style="display:inline;">${emp[i].deptName}</span>
+
+              <select class="form-select update${i}" style="display:none;" id="${i}deptID">
+                <option value="${emp[i].departmentID}" selected>${emp[i].deptName}</option>`;
+                for (k=0; k<depts.length; k++){
+                  if (emp[i].departmentID != depts[k].deptID){
+                    row += `<option value="${depts[k].deptID}">${depts[k].deptName}</option>`;
+                  }
+                }
+          row += `<option value="-1">NULL</option>
               </select>
-            </div>
+
           </td>
-          <td>${json[i].bool}</td>
-          <td><span class="spanrow${i}" style="display:inline;">"${json[i].catchPhrase}"</span>
-            <div class="input-group mb-3 update${i}" style="display:none;">
-              <select class="form-select" id="${i}quoteID">
-                <option value="${json[i].quoteID}" selected>"${json[i].catchPhrase}"</option>
-                <option value="-1">NULL</option>
+          <td>${emp[i].bool}</td>
+          <td><span class="spanrow${i}" style="display:inline;">"${emp[i].catchPhrase}"</span>
+
+              <select class="form-select update${i}" style="display:none;" id="${i}quoteID">
+                <option value="${emp[i].quoteID}" selected>"${emp[i].catchPhrase}"</option>`;
+                for (l=0; l<quotes.length; l++) {
+                  if (emp[i].quoteID != quotes[l].quoteID){
+                    row += `<option value="${quotes[l].quoteID}">"${quotes[l].catchPhrase}"</option>`;
+                  }
+                }
+          row += `<option value="-1">NULL</option>
               </select>
-            </div>
+
           </td>
-          <td>${json[i].employStatus}</td>
+          <td>${emp[i].employStatus}</td>
           <td>
           <input type="button" class="btn btn-outline-primary update" id="butUpdate${i}" value="Update" onclick="unlockEmployee(${i})" style="display:inline;">
-              <input type="button" class="btn btn-outline-primary save" id="butSave${i}" value="Save" onclick="updateEmployee(${i}, ${json[i].employeeID})" style="display:none;"></td>
-          <td><input type="button" class="btn btn-outline-primary delete" id="butDel${i}" value="Delete" onclick="delEmp(${json[i].employeeID})" style="display:inline;">
+              <input type="button" class="btn btn-outline-primary save" id="butSave${i}" value="Save" onclick="updateEmployee(${i}, ${emp[i].employeeID})" style="display:none;"></td>
+          <td><input type="button" class="btn btn-outline-primary delete" id="butDel${i}" value="Delete" onclick="delEmp(${emp[i].employeeID})" style="display:inline;">
           <input type="button" class="btn btn-outline-primary cancel" id='butCancel${i}' value="Cancel" onclick="lockEmployee(${i})" style="display:none;"></td>
         </tr>
         <tr scope="col">`;
